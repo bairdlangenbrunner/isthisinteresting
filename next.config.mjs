f609c2f4d411d2ac96839243b760723e36daa986
Baird Langenbrunner <bairdlangenbrunner@gmail.com>
2024-06-28 17:32:16 -0400

    changed next.config.mjs file to force static builds

--- next.config.mjs
+++ next.config.mjs
@@ -21,6 +21,7 @@ const withMDX = nextMDX({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   pageExtensions: ['ts','tsx','js','jsx','md','mdx'],
   experimental: {
   },
