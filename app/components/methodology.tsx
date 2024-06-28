8e45d139395a6f41dc90a8d7fdea47475ddd163a
Baird Langenbrunner <bairdlangenbrunner@gmail.com>
2024-06-28 17:09:42 -0400

    seems to be working now, ended up removing the layout.tsx of the (posts) directory and will mess with layout within the html code for now

--- app/components/methodology.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Methodology() {
-  return (
-  <div className="paragraph-widths mx-auto">
-    [import methodology content here]
-  </div>
-  )
-}
