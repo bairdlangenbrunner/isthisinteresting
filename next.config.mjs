a55ca86ead4d1c2e76df37b991e0c8a2dd8ac2a9
Baird Langenbrunner <bairdlangenbrunner@gmail.com>
2024-11-11 10:22:03 -0500

    updating with small changes, <br/> tags at end of posts to add more padding

--- next.config.mjs
+++ next.config.mjs
@@ -22,6 +22,15 @@ const withMDX = nextMDX({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+  
   pageExtensions: ['ts','tsx','js','jsx','md','mdx'],
   experimental: {
   },
